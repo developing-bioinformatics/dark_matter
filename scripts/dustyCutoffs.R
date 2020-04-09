5b4ba6e213c4f953c1526cf2d984e9e7f706747d
Eric Pederson <epederso@scc-jd2.scc.bu.edu>
2020-04-09 18:03:14 -0400

    Removed dependency for sratoolkit

--- scripts/dustyCutoffs.R
+++ scripts/dustyCutoffs.R
@@ -1,5 +1,6 @@
 #!/usr/bin/R
 library(rentrez)
+library(xml2)
 library(Biostrings)
 library(ggplot2)
 library(ShortRead)
@@ -8,15 +9,23 @@ library(stringr)
 options(scipen=999, digits=1) 
 
 function(srr, blasthits, cutoff=5000, amplicon_length=550, data=FALSE, verbose=TRUE){
-  #srr is an SRA accession number
-  #blasthits is a BLAST result data frame
+  #srr is an SRA accession number (string)
+  #blasthits is a BLAST result data frame (before or after taxonomizr)
   #cutoff is a number from 1 to maximum dusty score of srr. 
-  #Default cutoff=5000 
+  #Default cutoff = 5000 
   #If cutoff is NULL, kmeans clustering is used
-  #If data = TRUE, a data frame with quality scores, complexity scores and cluster ids with length of dna.
+  #If data = TRUE, the function returns a data frame with quality scores, complexity scores and cluster ids with length of dna .
   #If data = FALSE, the function returns a ggplot object.
   
-  system(paste('fastq-dump', srr, sep=' ')) 
+  if (file.exists(paste(srr,'.fastq',sep=''))==FALSE) {
+    #system(paste('fastq-dump', srr, sep=' '))
+    SRAfile <- read_xml(entrez_fetch(db="sra",id=srr,rettype='xml')) %>%
+      xml_find_all("//RUN_SET") %>% 
+      xml_find_all("//SRAFile") %>% 
+      xml_attr('url')
+    download.file(SRAfile[1], paste(srr,'.fastq',sep=""))
+  }
+  
   dna = readFastq('.', pattern=srr)
   if (verbose==TRUE){
     cat('Extracting metadata\n')
@@ -25,16 +34,18 @@ function(srr, blasthits, cutoff=5000, amplicon_length=550, data=FALSE, verbose=T
   reads = sread(dna)
   qscores = quality(dna) 
   escore = max(cl$E)
+  
   if (verbose==TRUE){
     cat('Splitting into reads with and without BLAST hits\n')
   }
+  
   #extract read indices
   regexp <- "[[:digit:]]+"
   idxHits = as.numeric(unique(str_extract(blasthits$QueryID, regexp)))
   idxNA = unique(setdiff((seq(1:length(dna))),idxHits))
   NA_reads = dna[idxNA]
 
-############ Quality scores of Hits
+## Quality scores of Hits
   if (verbose==TRUE){
     cat('Calculating quality scores for each read\n')
   }
@@ -69,31 +80,33 @@ function(srr, blasthits, cutoff=5000, amplicon_length=550, data=FALSE, verbose=T
     if (verbose==TRUE){
       cat('K-means clustering into high and low complexity populations\n')
     }
-  clusterHits = kmeans(meanQscores_dustyHits,2)
-  meanQscores_dustyHits$cluster <- as.factor(clusterHits$cluster)
-  clusterCentersHits <- as.data.frame(clusterHits$centers,clusterHits$cluster)
+    
+    clusterNA = kmeans(meanQscores_dustyNA,2)
+    meanQscores_dustyNA$cluster <- as.factor((clusterNA$cluster+2))
+    clusterCentersNA <- as.data.frame(clusterNA$centers,clusterNA$cluster)
+  
+    clusterHits = kmeans(meanQscores_dustyHits,2)
+    meanQscores_dustyHits$cluster <- as.factor(clusterHits$cluster)
+    clusterCentersHits <- as.data.frame(clusterHits$centers,clusterHits$cluster)
 
-  clusterNA = kmeans(meanQscores_dustyNA,2)
-  meanQscores_dustyNA$cluster <- as.factor((clusterNA$cluster+2))
-  clusterCentersNA <- as.data.frame(clusterNA$centers,clusterNA$cluster)
   } else {
     if (verbose==TRUE){
       cat(paste('Grouping into high and low complexity populations with cutoff = ',cutoff,"\n",sep=""))
     }
-  clusterHits <- as.numeric((meanQscores_dustyHits$Dusty < cutoff)+1)
-  meanQscores_dustyHits$cluster <- as.factor(clusterHits)
-  clusterCentersHits <- as.data.frame(rbind(c(Q=mean(meanQscores_dustyHits$meanQ[meanQscores_dustyHits$cluster==1]), D=mean(meanQscores_dustyHits$Dusty[meanQscores_dustyHits$cluster==1])),c(Q=mean(meanQscores_dustyHits$meanQ[meanQscores_dustyHits$cluster==2]), D=mean(meanQscores_dustyHits$Dusty[meanQscores_dustyHits$cluster==2]))))
+    clusterHits <- as.numeric((meanQscores_dustyHits$Dusty < cutoff)+1)
+    meanQscores_dustyHits$cluster <- as.factor(clusterHits)
+    clusterCentersHits <- as.data.frame(rbind(c(Q=mean(meanQscores_dustyHits$meanQ[meanQscores_dustyHits$cluster==1]), D=mean(meanQscores_dustyHits$Dusty[meanQscores_dustyHits$cluster==1])),c(Q=mean(meanQscores_dustyHits$meanQ[meanQscores_dustyHits$cluster==2]), D=mean(meanQscores_dustyHits$Dusty[meanQscores_dustyHits$cluster==2]))))
   
-  clusterNA <- as.numeric((meanQscores_dustyNA$Dusty < cutoff)+3)
-  meanQscores_dustyNA$cluster <- as.factor((clusterNA))
-  clusterCentersNA <- as.data.frame(rbind(c(Q=mean(meanQscores_dustyNA$meanQ[meanQscores_dustyNA$cluster==3]), D=mean(meanQscores_dustyNA$Dusty[meanQscores_dustyNA$cluster==3])),c(Q=mean(meanQscores_dustyNA$meanQ[meanQscores_dustyNA$cluster==4]), D=mean(meanQscores_dustyNA$Dusty[meanQscores_dustyNA$cluster==4]))))
+    clusterNA <- as.numeric((meanQscores_dustyNA$Dusty < cutoff)+3)
+    meanQscores_dustyNA$cluster <- as.factor((clusterNA))
+    clusterCentersNA <- as.data.frame(rbind(c(Q=mean(meanQscores_dustyNA$meanQ[meanQscores_dustyNA$cluster==3]), D=mean(meanQscores_dustyNA$Dusty[meanQscores_dustyNA$cluster==3])),c(Q=mean(meanQscores_dustyNA$meanQ[meanQscores_dustyNA$cluster==4]), D=mean(meanQscores_dustyNA$Dusty[meanQscores_dustyNA$cluster==4]))))
   }
 
-  
   #Overlayed plot
   if (verbose==TRUE){
     cat('Building plot of all clusters\n')
   }
+  
   histcomb_hits_label  <- paste("BLAST Hits (",length(idxHits),")", sep="")
   histcomb_NA_label  <- paste("No BLAST Hits (",length(idxNA),")", sep="")
   histcomb = (ggplot(meanQscores_dustyNA) + 
@@ -131,12 +144,14 @@ function(srr, blasthits, cutoff=5000, amplicon_length=550, data=FALSE, verbose=T
     obj_out = setNames(data.frame(matrix(ncol=3,nrow=length(dna))),c("meanQ","Dusty","cluster"))
     obj_out[,] = as.numeric(0)
 
-    obj_out$meanQ[idxHits] <- meanQscores_dustyHits$meanQ
-    obj_out$Dusty[idxHits] <- meanQscores_dustyHits$Dusty
-    obj_out$cluster[idxHits] <- as.numeric_version(meanQscores_dustyHits$cluster)
     obj_out$meanQ[idxNA] <- meanQscores_dustyNA$meanQ
     obj_out$Dusty[idxNA] <- meanQscores_dustyNA$Dusty
     obj_out$cluster[idxNA] <- as.numeric_version(meanQscores_dustyNA$cluster)
+    
+    obj_out$meanQ[idxHits] <- meanQscores_dustyHits$meanQ
+    obj_out$Dusty[idxHits] <- meanQscores_dustyHits$Dusty
+    obj_out$cluster[idxHits] <- as.numeric_version(meanQscores_dustyHits$cluster)
+
     if (verbose==TRUE){
       cat('Returning data frame\n')
     }
